b225dfd7ac072c19754bd5213bb8f82a3c017a36
ABDULRAHMAN AL-MYMAN_عبدالرحمن الميمان <66333040+PYTHON01100100@users.noreply.github.com>
2021-09-12 20:08:00 +0300

    Add files via upload

---
+++ Code/tut1/HelloWorld.java
@@ -0,0 +1,12 @@
+package tut1;
+
+public class HelloWorld 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi"); //ln new line after print
+		System.out.print("Welcome to CSC111.");
+		System.out.print("This is a great course.");
+		System.out.print(";)");
+	}
+}
---
+++ Code/tut1/Q1.java
@@ -0,0 +1,39 @@
+package tut1;
+
+public class Q1 
+{
+	public static void main(String[] args) 
+	{
+		/*
+		int x; //Define
+		x = 8; //Assign
+		//int a = 10;
+
+		int xyz = 100; //Define and Assign
+
+		String hi_all = "Hi all";
+		
+		//int a = 10, d = 9, k = 7;
+		int a = 10;
+		int d = 9;
+		int k = 7;
+		
+		int y,h,j,p;
+		
+		//double addResult = xyz + x;
+		double addResult;
+		addResult = xyz + x;
+		*/
+		
+		//char is 2 bytes
+		char c1 = 'a'; //0 - 65000
+		//char c2 = 97;
+		System.out.println(c1);
+		c1 = '+';
+		c1 = '1'; //character 1 , not the value 1
+		
+		String s1 = ""; //Empty String
+		
+		
+	}
+}
