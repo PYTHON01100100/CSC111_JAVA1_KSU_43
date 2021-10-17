a563399c3cda99e31df7110bf397f87f48db2fda
ABDULRAHMAN AL-MYMAN_عبدالرحمن الميمان <66333040+PYTHON01100100@users.noreply.github.com>
2021-10-17 12:56:50 +0300

    Add files via upload

---
+++ CODES_train/com_text.java
@@ -0,0 +1,48 @@
+package train;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+import java.math.*;
+import java.lang.Math;
+public class Train {
+ 	public static void main(String[] args) {
+ 		Scanner input=new Scanner(System.in).useLocale(Locale.US);
+ 		System.out.println("enter s1 and s2");
+ 		String s1=input.next();
+ 		String s2=input.next();
+ 		System.out.println("[1]sne \t [2]insen");
+ 		System.out.println("options:");
+ 		int nop=input.nextInt();
+ 		boolean b=true;
+ 		do {
+ 			if(nop!=1&&nop!=2) {
+ 				System.out.println("wrong choice try again");
+ 		 		System.out.println("[1]sne \t [2]insen");
+ 		 		System.out.println("options:");
+ 		 		nop=input.nextInt();
+ 		 		
+ 			}
+ 			if(nop==1) {
+ 				if(s1.equals(s2)) {
+ 					System.out.println("equals");
+ 					b=false;
+ 					}
+ 				else {
+ 					System.out.println("not equals");
+ 				    b=false;
+ 				    }
+ 				}
+ 			else if(nop==2) {
+ 				if(s1.equalsIgnoreCase(s2)) {
+ 					System.out.println("equals");
+ 					b=false;
+ 					}
+ 				else{
+ 					System.out.println("not equals");
+  				   b=false;
+ 				   }
+ 				}
+
+ 		}while(b);
+ 		}
+ 	}
