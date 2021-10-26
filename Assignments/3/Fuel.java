a91adba250ada2521111ef0cefb6752b0810cb3b
ABDULRAHMAN AL-MYMAN_عبدالرحمن الميمان <66333040+PYTHON01100100@users.noreply.github.com>
2021-10-26 10:14:20 +0300

    Add files via upload

---
+++ Assignments/3/Fuel.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class Fuel {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in).useLocale(Locale.US);
+		
+		System.out.print("Enter the driving distance: ");
+		double Distance = input.nextDouble();
+		System.out.print("Enter kilos per liter:");
+		double KPL = input.nextDouble();
+		System.out.print("Enter price per liter: ");
+		double PPL = input.nextDouble();
+		double cost =Distance/KPL*PPL;
+		System.out.println("The cost of driving is $"+cost);
+		
+		
+		
+	}
+
+}
